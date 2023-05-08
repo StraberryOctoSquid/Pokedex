bce8a1da5ad390febdb952b98f84fc853a31227a
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-05-08 16:45:49 -0500

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const pokemonList = {
+name:"mewtwo", height:"5",types: ["darkness", "psychic"], 
+name:"charizard", height:"15",types: ["fire"], 
+name:"wartortle", height:"5",types: ["water", "grass"] }
