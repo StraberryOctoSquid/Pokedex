4963ce1f8528feb296016ad1346a3a36f771a7ab
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-05-09 08:40:41 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-const pokemonList = {
-name:"mewtwo", height:"5",types: ["darkness", "psychic"], 
-name:"charizard", height:"15",types: ["fire"], 
-name:"wartortle", height:"5",types: ["water", "grass"] }
+let pokemonList = [ 
+    {
+        name: "mewtwo", height:5, types: ["psychic"]
+     },
+     {
+        name:"charizard", height: 15, types: ["fire"]
+    },
+    {
+        name:"wartortle", height:5,types: ["water", "grass"]
+    }
+
+]
+console.log(pokemonList)
