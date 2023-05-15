beda2defd80df75ba507a68a8d1d7b56928db9b5
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-05-15 13:00:47 -0500

    Squashed commit of the following:
    commit 42d62d1dd2f8814ce757d73f8a318b7a5de7d3c0
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Fri May 12 07:03:56 2023 -0500
    
        Update index.html
    
    commit 9c3f86f484c352b5475ec35c399f01a6ba4158a4
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Wed May 10 14:15:04 2023 -0500
    
        comment
    
        insert tall comment about pokemon >10
    
    commit eff0a36f4e09bb3efe8fc77aace13197a77f70bf
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Wed May 10 14:14:20 2023 -0500
    
        provide structure for css
    
    commit dffa2cf2505f386a5aa65374182d353732a93c4d
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Wed May 10 14:13:32 2023 -0500
    
        Update styles.css
    
    commit 85932300290a4607d8fd218897cfc66eb17cb1bb
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Wed May 10 10:26:29 2023 -0500
    
        minimal formatting to pokelist
    
    commit 4963ce1f8528feb296016ad1346a3a36f771a7ab
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Tue May 9 08:40:41 2023 -0500
    
        Update scripts.js
    
    commit bce8a1da5ad390febdb952b98f84fc853a31227a
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Mon May 8 16:45:49 2023 -0500
    
        Update scripts.js
    
    commit a61cfe015bc00c989f5d624d35c6cdec46b9b879
    Author: StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
    Date:   Mon May 8 10:46:46 2023 -0500
    
        clean slate
    
        delete all script to start exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-let favoriteFood='Detroit Pizza';
-document.write(favoriteFood);
+
+//sets up a variable of an array of pokemon with name, height and type attributes
+let pokemonList = [ 
+    {
+        name: "Mewtwo", height:5, types: ["psychic"]
+     },
+     {
+        name:"Charizard", height: 15, types: ["fire"]
+    },
+    {
+        name:"Wartortle", height:5,types: ["water", "grass"]
+    }
+
+];
+
+
+for (let i=0; i <pokemonList.length; i++) {
+    document.write('<p class=pokemon>' + pokemonList[i].name + ": Height:" + pokemonList[i].height);
+    if (pokemonList[i].height >10) {
+        document.write(" :Wow that's tall!")
+    }
+}
