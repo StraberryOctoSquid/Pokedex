d5e3caab1e0f815ff62c2399982321ea21fd699f
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-05-19 10:13:54 -0500

    change .js file name and link

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,78 @@
 
-//sets up a variable of an array of pokemon with name, height and type attributes
-let pokemonList = [ 
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
-        name: "Mewtwo", height:5, types: ["psychic"]
-     },
-     {
-        name:"Charizard", height: 15, types: ["fire"]
+      name: 'Mewtwo',
+      height: '5\' 5"',
+      types: ['psychic'],
+      category: "genetic"
     },
     {
-        name:"Wartortle", height:5,types: ["water", "grass"]
-    }
+      name: 'Charizard',
+      height: 15,
+      types: ['fire'],
+      category: "flame"
+    },
+    {
+      name: 'Wortle',
+      height: 5,
+      types: ['water', 'grass'],
+      category: "turtle"
+    },
 
-];
+  ];
 
-Object.keys(pokemonList).forEach(function (property) {
-    document.write(pokemonList[property]);
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
     
+    // event listner that ennacts showDetails function
+    button.addEventListener('click',function (event) {
+      showDetails(pokemon);
+    })
+
+  }
+  // logs the name of the pokemon of the button that is clicked
+  function showDetails(pokemon) {
+    console.log(pokemon.name)
+    
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
+
+})();
+
+pokemonRepository.add({
+  name: 'Charmander',
+  height: 2,
+  types: ['fire'],
+  category: "lizard"
+})
+
+
+document.write("Pokédex ");
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+
+  pokemonRepository.addListItem(pokemon);
+
 });
-
-// pokemonList.forEach( item => document.write(name,height,types,));
-
-
-// document.write('<h3>===forEach LOOP===</h3>');
-// pokemonList.forEach(function (currentItem, index) {
-//   document.write('<p>' + index + '</p>');
-// });
-
-
-// document.write 
-// pokemonList.forEach(function (currentItem) {
-//     document.write('<p>' + currentItem + '</p>');
-    
-    
-// });
-
-// for (let i=0; i <pokemonList.length; i++) {
-//     document.write('<p class=pokemon>' + pokemonList[i].name + ": Height:" + pokemonList[i].height);
-//     if (pokemonList[i].height >10) {
-//         document.write(" :Wow that's tall!")
-//     }
-// }
