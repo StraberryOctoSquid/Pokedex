b21aadfdc9746665ec340abbd25ac2e2d46eb869
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-05-29 07:19:03 -0500

    add many notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-
+// container function of iife
 let pokemonRepository = (function () {
+  
+  // define pokemonLIst as an array
   let pokemonList = [];
+
+  // define apiURL as the pokemon database api https address
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  // what does this function do? is it a promise?
+  // if the pokemon is an object and has a name it will then be pushed to pokemonList, else console.log "...."?
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -13,12 +19,22 @@ let pokemonRepository = (function () {
       console.log("pokemon is not correct");
     }
   }
+  
+  // create getAll function that returns pokemonList array
   function getAll() {
     return pokemonList;
   }
+
+  //creates button with text of pokemon.name, and click event listner
   function addListItem(pokemon) {
+    
+    // defines pokemonList as ul.pokemon-list
     let pokemonList = document.querySelector(".pokemon-list");
+
+    // creates list item defined as listPokemon
     let listPokemon = document.createElement('li');
+    
+    // creates button defined as button with text pokemon.name
     let button = document.createElement('button');
     button.innerText = pokemon.name;
 
@@ -29,13 +45,18 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listPokemon);
 
     // event listner that ennacts showDetails function
+    // enacts showDetails and showModal functions
     button.addEventListener('click', function (event) {
       showDetails(pokemon);
+      showModal(title, text);
     })
 
 
   }
  
+  // 
+  
+
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -87,3 +108,4 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
